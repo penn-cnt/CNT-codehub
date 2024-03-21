1957bc5f5ab9e193470aab1e21ecb3435c22ef1f
UPennBJPrager <bjprager@seas.upenn.edu>
2024-03-21 08:22:42 -0400

    Update data_merge.py

--- scripts/slowing/data_merge.py
+++ scripts/slowing/data_merge.py
@@ -12,4 +12,10 @@ if __name__ == '__main__':
 
     # Find all the feature dataframes
     files = glob(f"{rootdir}*features*pickle")
-    print(files)
+    
+    # Read in the mapping file from yaml
+    yaml_dict = yaml.safe_load(open(argv[2],'r'))
+
+    # Make the output dataframe object
+    output = PD.DataFrame(columns=yaml_dict.keys())
+    print(output)
